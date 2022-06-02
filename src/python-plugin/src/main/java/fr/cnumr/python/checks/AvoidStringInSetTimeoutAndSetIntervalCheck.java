5b702fc822701f23792e414a616e3d08946c8049
Mikael Picard <mpicard@aubay.com>
2022-06-02 12:35:39 +0200

    Adding Rule without initialize

--- src/python-plugin/src/main/java/fr/cnumr/python/checks/AvoidStringInSetTimeoutAndSetIntervalCheck.java
+++ src/python-plugin/src/main/java/fr/cnumr/python/checks/AvoidStringInSetTimeoutAndSetIntervalCheck.java
@@ -1,13 +1,31 @@
 package fr.cnumr.python.checks;
 
+import org.sonar.check.Priority;
+import org.sonar.check.Rule;
 import org.sonar.plugins.python.api.PythonSubscriptionCheck;
+import org.sonar.plugins.python.api.SubscriptionContext;
+import org.sonar.plugins.python.api.tree.TryStatement;
 
+@Rule(
+        key = AvoidStringInSetTimeoutAndSetIntervalCheck.RULE_KEY,
+        name = "Developpement",
+        description = AvoidStringInSetTimeoutAndSetIntervalCheck.DESCRIPTION,
+        priority = Priority.MINOR,
+        tags = {"bug"})
 public class AvoidStringInSetTimeoutAndSetIntervalCheck extends PythonSubscriptionCheck {
 
-	@Override
-	public void initialize(Context arg0) {
-		// TODO Auto-generated method stub
-		
-	}
+    public static final String RULE_KEY = "S52";
+    public static final String DESCRIPTION = "Avoid strings as arguments to SetTimeout() and setInterval()";
+
+    @Override
+    public void initialize(Context context) {
+        // TODO
+    }
+
+    public void visitNode(SubscriptionContext ctx) {
+        TryStatement tryStatement = (TryStatement) ctx.syntaxNode();
+        ctx.addIssue(tryStatement.tryKeyword(), DESCRIPTION);
+
+    }
 
 }
