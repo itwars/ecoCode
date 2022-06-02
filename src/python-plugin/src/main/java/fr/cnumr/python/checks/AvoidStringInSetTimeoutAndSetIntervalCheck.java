0ddbd40688cd0f9250cdc4695fa32d03e09db585
Mikael Picard <mpicard@aubay.com>
2022-06-02 12:17:06 +0200

    Initial commit for rule D5 Python

---
+++ src/python-plugin/src/main/java/fr/cnumr/python/checks/AvoidStringInSetTimeoutAndSetIntervalCheck.java
@@ -0,0 +1,13 @@
+package fr.cnumr.python.checks;
+
+import org.sonar.plugins.python.api.PythonSubscriptionCheck;
+
+public class AvoidStringInSetTimeoutAndSetIntervalCheck extends PythonSubscriptionCheck {
+
+	@Override
+	public void initialize(Context arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
